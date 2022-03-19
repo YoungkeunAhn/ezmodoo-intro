b0800974072d300e9ab08d477f446fb1f2ae31e5
YounkeunAhn <tosan2094@gmail.com>
2022-03-19 17:32:40 +0900

    초기설정 & 메뉴 타입 정리 & 테스트 배너

---
+++ src/types/type.d.ts
@@ -0,0 +1,9 @@
+declare type MenuType = {
+  title: string
+  subTitle: string[]
+}
+
+declare type BannerType = {
+  image: string
+  content: string
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'], //파일 크기를 최소화하기 위한 옵션, 여기에 설정된 파일이 아니면 삭제된다.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
