3be78c0e7b9a94c61df0eb9d51c27039f6d967b0
YounkeunAhn <tosan2094@gmail.com>
2022-03-19 17:38:50 +0900

    테스트 배너 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { banner } from 'data/menu'
 import React from 'react'
 
 function App() {
   return (
     <div className='App'>
       <div className='text-blue-600'>Hello react with tailwind</div>
+      <img src={banner[0].image} alt='' />
     </div>
   )
 }
