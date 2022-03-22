f57f6a9bd2a6bda7742fa368f6b7e584c4ea8499
YounkeunAhn <tosan2094@gmail.com>
2022-03-22 14:40:24 +0900

    풀필먼트

--- src/asset/ChevronRight.tsx
+++ src/asset/ChevronRight.tsx
@@ -10,7 +10,9 @@ function ChevronRight(props: Props) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      className={`h-6 w-6 rounded-full ml-2 ${white ? 'bg-black' : 'bg-white'}`}
+      className={`rounded-full ml-2 ${white ? 'bg-black' : 'bg-white'} ${
+        white ? 'h-4 w-4' : 'h-6 w-6'
+      }`}
       fill='none'
       viewBox='0 0 24 24'
       stroke={white ? '#fff' : '#000'}
