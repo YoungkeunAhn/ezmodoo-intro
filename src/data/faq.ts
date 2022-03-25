e3b6af0fcc46c2ca66962c50c744563fd235409e
YounkeunAhn <tosan2094@gmail.com>
2022-03-25 14:37:05 +0900

    faq이미지, 카테고리 데이터 추가

---
+++ src/data/faq.ts
@@ -0,0 +1,30 @@
+export const searchCategory = [
+  {
+    icon: 'faq_cate_all.png',
+    category: '전체',
+  },
+  {
+    icon: 'faq_cate_site.png',
+    category: '사이트이용',
+  },
+  {
+    icon: 'faq_cate_error.png',
+    category: '오류해결',
+  },
+  {
+    icon: 'faq_cate_service.png',
+    category: '서비스기능',
+  },
+  {
+    icon: 'faq_cate_extra.png',
+    category: '부가서비스',
+  },
+  {
+    icon: 'faq_cate_policy.png',
+    category: '정책관련',
+  },
+  {
+    icon: 'faq_cate_setting.png',
+    category: '설치방법',
+  },
+]
