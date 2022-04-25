6c3f8076287cd9f47ecec8948d5eabd9e37050aa
YounkeunAhn <tosan2094@gmail.com>
2022-04-25 16:54:15 +0900

    경고메세지 수정

--- src/common/login-box/LoginBox.tsx
+++ src/common/login-box/LoginBox.tsx
@@ -21,7 +21,7 @@ function LoginBox(props: Props) {
   const { open } = props
 
   const [inputs, setInputs] = useState<InputsType>(initInputs)
-  const [loginSave, setLoginSave] = useState<boolean>(false)
+  const [isLoginSave, setIsLoginSave] = useState<boolean>(false)
   const [actionUrl, setActionUrl] = useState<string>('')
 
   const onChangeInputs = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -91,10 +91,9 @@ function LoginBox(props: Props) {
                 type='checkbox'
                 className='form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer'
                 id='loginSave'
-                name='loginSave'
-                checked={loginSave}
+                checked={isLoginSave}
                 onChange={(event) => {
-                  setLoginSave(event.target.checked)
+                  setIsLoginSave(event.target.checked)
                 }}
               />
               <label
