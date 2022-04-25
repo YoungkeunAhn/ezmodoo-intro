1a089fb3f63c0ff89e7e70d41a89e5d42cf460e1
YounkeunAhn <tosan2094@gmail.com>
2022-04-25 16:39:44 +0900

    404페이지 메인으로 이동 버튼 추가

--- src/pages/error404/Error404.tsx
+++ src/pages/error404/Error404.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Error404() {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
       <h1 className='text-2xl font-bold'>404 Not found</h1>
+      <Link to='/'>
+        <button className='mt-5 rounded-md text-sm font-bold p-2 bg-[#E3EAF1]'>
+          메인페이지로 이동
+        </button>
+      </Link>
     </div>
   )
 }
